0e1c51fee6317098e762496616d1ada0dafbf9ab
Daniel Murphy <daniel.murphy@capco.com>
2022-03-02 22:53:37 -0500

    Created the DB modal and updated routes to use it - added middleware error handling and Mock data to use in postman and send into MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env variables
@@ -14,6 +16,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body Parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -22,17 +27,23 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// use errorHandler Middleware
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`
+      .brightMagenta.bold
+  )
 );
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log('process.on');
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit processs
   server.close(() => process.exit(1));
 });
